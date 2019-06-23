c8b6cc3eeccc827d4472bbf605bcfc7bf2c8e5e4
CryptDo3 <meltonkl@icloud.com>
2019-06-23 18:16:17 -0400

    Added correct license block to top of file.

--- AppDataTypes/Alert.py
+++ AppDataTypes/Alert.py
@@ -1,3 +1,18 @@
+#  ProcMonD-Prototype - A simple daemon for monitoring running processes for suspicious behavior.
+#  Copyright (C) 2019  Krystal Melton
+#
+#  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+#  later version.
+#
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#  details.
+#
+#  You should have received a copy of the GNU General Public License along with this program.  If not,
+#  see <https://www.gnu.org/licenses/>.
+#
+
 class Alert:
     """
     The Alert class encapsulates the metadata for a suspicious event.
